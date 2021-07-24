c7d7e9fd539b529fd62d2d47d2b9e9f8bde95590
Aydin Nasrollahpour <68078247+awydini@users.noreply.github.com>
2021-07-24 08:26:41 +0430

    refactore

--- src/main/java/net/aydini/jawpdater/runner/AbstractUpdaterExecuter.java
+++ src/main/java/net/aydini/jawpdater/runner/AbstractUpdaterExecuter.java
@@ -1,11 +1,6 @@
 package net.aydini.jawpdater.runner;
 
-import java.io.File;
-import java.util.Optional;
-
-import lombok.extern.slf4j.Slf4j;
 import net.aydini.jawpdater.script.ScriptPath;
-import net.aydini.jawpdater.util.UserOs;
 
 /**
  * 
@@ -13,51 +8,30 @@ import net.aydini.jawpdater.util.UserOs;
  *
  */
 
-@Slf4j
 public abstract class AbstractUpdaterExecuter extends ScriptExecuter implements JawpdaterExecuter {
 
-
 	protected abstract ScriptPath getScriptPath();
-	
-	protected abstract CommandExecuter getCommandExecuter(final File scriptFile ); 
+
 
 	@Override
 	public final void executeBackup() {
-		execute(createCommandExecuter(getScriptPath().getExecuteBackupScript()));
+		execute(getScriptPath().getExecuteBackupScript());
 	}
 
 	@Override
 	public final void executeDeleteBackup() {
-		execute(createCommandExecuter(getScriptPath().getExecuteDeleteBackup()));
+		execute(getScriptPath().getExecuteDeleteBackup());
 	}
 
 	@Override
 	public final void executeUndoBackup() {
-		execute(createCommandExecuter(getScriptPath().getExecuteUndoBackup()));
-		
+		execute(getScriptPath().getExecuteUndoBackup());
+
 	}
 
 	@Override
-	public final  void executeApplication() {
-		execute(createCommandExecuter(getScriptPath().getExecuteApplication()));
+	public final void executeApplication() {
+		execute(getScriptPath().getExecuteApplication());
 	}
-	
-	private CommandExecuter createCommandExecuter(final String scriptFilePath)
-	{
-		final File scriptFile = createExecutableScriptFile(scriptFilePath).orElseThrow(()->new RuntimeException( scriptFilePath +" file not found"));
-		return getCommandExecuter(scriptFile);
-	}
-	
-	private Optional<File> createExecutableScriptFile(String scriptFilePath)
-	{
-		try {
-		File file = new File("bin"+ new UserOs().getPathSeperator()+scriptFilePath);
-		file.setExecutable(true, false);
-		log.info(file.getAbsolutePath());
-		return Optional.of(file);
-		} catch (final Exception e) {
-			log.error("error making file executable {} ",e.getMessage());
-		}
-		return Optional.empty();
-	}
+
 }
