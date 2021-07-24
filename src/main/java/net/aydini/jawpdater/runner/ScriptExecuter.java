a7d353925de0a6be4509b15944b70ff83d44ffaa
Aydin Nasrollahpour <68078247+awydini@users.noreply.github.com>
2021-07-24 08:27:34 +0430

    refactor

--- src/main/java/net/aydini/jawpdater/runner/ScriptExecuter.java
+++ src/main/java/net/aydini/jawpdater/runner/ScriptExecuter.java
@@ -1,6 +1,10 @@
 package net.aydini.jawpdater.runner;
 
+import java.io.File;
+import java.util.Optional;
+
 import lombok.extern.slf4j.Slf4j;
+import net.aydini.jawpdater.util.UserOs;
 
 /**
  * 
@@ -11,16 +15,38 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class ScriptExecuter {
 
+	protected abstract CommandExecuter getCommandExecuter(final File scriptFile);
 	/**
 	 * executes script
 	 * 
 	 * @param scriptFilePath
 	 */
-	protected void execute(final CommandExecuter commandExecuter) {
+	protected void execute(final String  scriptFilePath) {
 		try {
-			commandExecuter.execute();
+			createCommandExecuter(scriptFilePath).execute();
 		} catch (final Exception e) {
 			log.error("error starting program {} ", e.getMessage());
 		}
 	}
+	
+	
+	private CommandExecuter createCommandExecuter(final String scriptFilePath)
+	{
+		final File scriptFile = createExecutableScriptFile(scriptFilePath).orElseThrow(()->new RuntimeException( scriptFilePath +" file not found"));
+		return getCommandExecuter(scriptFile);
+	}
+	
+	private Optional<File> createExecutableScriptFile(String scriptFilePath)
+	{
+		try {
+		File file = new File("bin"+ new UserOs().getPathSeperator()+scriptFilePath);
+		file.setExecutable(true, false);
+		log.info(file.getAbsolutePath());
+		return Optional.of(file);
+		} catch (final Exception e) {
+			log.error("error making file executable {} ",e.getMessage());
+		}
+		return Optional.empty();
+	}
+
 }
